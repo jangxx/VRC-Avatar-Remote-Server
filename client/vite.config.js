76721e6576038ec95b77c6c6afd65b6a33245dec
jangxx <janscheiper95@gmail.com>
2022-02-27 17:56:24 +0100

    added vue3 and vite boilerplate

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "admin.html")
+      }
+    }
+  }
+})
