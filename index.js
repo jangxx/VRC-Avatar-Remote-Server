3bf19d3f3ad5d50b337bee3198a504ef0f19861d
jangxx <janscheiper95@gmail.com>
2022-03-19 20:01:39 +0100

    disabled initial test data emission

--- index.js
+++ index.js
@@ -439,13 +439,14 @@ async function main() {
 	avatarManager.init();
 	socketManager.init();
 
-	// for testing and development only
-	setTimeout(() => {
-		oscManager.emit("message", {
-			address: "/avatar/change", 
-			value: "avtr_418bf257-d957-46c9-be51-bf97ac25b862",
-		});
-	}, 1000);
+	if ("TEST_TRIGGER" in process.env) {
+		setTimeout(() => {
+			oscManager.emit("message", {
+				address: "/avatar/change", 
+				value: "avtr_418bf257-d957-46c9-be51-bf97ac25b862",
+			});
+		}, 1000);
+	}
 }
 
 main().catch(err => {
